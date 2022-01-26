39d7d3cdd3fd74501742c2204a911713ab9b9d74
Moutusi304 <83460447+Moutusi304@users.noreply.github.com>
2022-01-26 16:12:04 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,22 @@
+#Add implementation
+def add (x,y):
+	return x+y
+
+#Subtract implementation
+def subtract(x,y):
+	return x-y				#on master branch
+
+#Multiply implementation	
+def multiply(x,y):
+	return x*y 				#on bug456 branch
+
+#Divide implementation
+def divide(x,y):			#on master branch
+	if(y==0):
+		return DIVIDE_BY_0_ERROR
+	else:
+		return x/y
+
+#Square implementation		
+def square(x):
+	return x*x
